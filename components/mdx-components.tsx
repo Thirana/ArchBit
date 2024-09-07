848430996ca0cab41d1849aa0a26718613807db0
Thirana <t.embuldeniya@gmail.com>
2024-09-07 07:15:17 +0530

    added custom mdx read functinality

---
+++ components/mdx-components.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import * as runtime from "react/jsx-runtime";
+import { cn } from "@/lib/utils";
+
+//compiles the MDX string into a React component.
+const useMDXComponent = (code: string) => {
+  const fn = new Function(code);
+  return fn({ ...runtime }).default;
+};
+
+const components = {
+  Image,
+};
+
+interface MdxProps {
+  code: string;
+}
+
+// renders the compiled component, passing custom components like Image for use inside the MDX.
+export function MDXContent({ code }: MdxProps) {
+  const Component = useMDXComponent(code);
+  return <Component components={components} />;
+}
